8f584f09a6d2e7cca26693c7c5e80e00ebd3d921
ishan-053 <shindeishan042@gmail.com>
2026-02-28 22:29:23 +0530

    Switch to bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
