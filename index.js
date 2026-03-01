fccfbcebdb4b14a0b5ef2cd1d0dbdf09d454ad3f
ishan-053 <shindeishan042@gmail.com>
2026-03-01 12:36:50 +0530

    fix db not defined

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(passport.session());
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
